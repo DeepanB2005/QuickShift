5aad712faa4feada857fadd61e37089245d258ce
DeepanB2005 <deepanb2005@gmail.com>
2025-08-25 19:22:07 +0530

    cross resource

--- bc/src/server.js
+++ bc/src/server.js
@@ -14,13 +14,8 @@ const app = express();
 
 app.use(helmet());
 app.use(express.json());
-app.use(cors({
-  origin: [
-    process.env.FRONTEND_URL, // uses .env value
-    "http://localhost:5173"   // for local development
-  ],
-  credentials: false
-}));
+app.use(cors({ origin: '*' }));
+
 
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 300 });
 app.use(limiter);
